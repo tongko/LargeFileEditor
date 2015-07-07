d050420ca3deeb7d59506a4111f1cec73e49f2d3
Terry Liew <terry.liew@ivisgroup.com>
2015-07-07 21:16:06 +0800

    Added some features.

--- TextEditor.Core/Document.cs
+++ TextEditor.Core/Document.cs
@@ -6,21 +6,24 @@ namespace TextEditor.Core
 {
 	public class Document : IDisposable
 	{
-		private const int OffsetFactor = 32768;		//	32K
+		//private const int OffsetFactor = 32768;		//	32K
 		private const int MemBlockSize = 65536;	//	64K
 		private SpanTable _spanTable;
 		private MemoryMappedFile _mapped;
 		private MemoryMappedViewStream _stream;
 		private FileStream _editBuffer;
-		private int _viewIndex;
+		//private int _viewIndex;
 		private long _position;
 
 		public Document()
 		{
 			TagId = Guid.NewGuid();
-			_viewIndex = -1;
+			//_viewIndex = -1;
 			_position = 0;
+			BufferSize = 4096;
 		}
+
+		public int BufferSize { get; set; }
 
 		public Guid TagId { get; private set; }
 
@@ -29,6 +32,9 @@ namespace TextEditor.Core
 		public int Read(byte[] buffer, int index, int count)
 		{
 			Span span;
+			if (count > MemBlockSize)
+				count = MemBlockSize;
+
 			var start = _spanTable.Find(_position, out span);
 			if (start < 0)
 				return 0;
@@ -36,7 +42,8 @@ namespace TextEditor.Core
 			var c = count;
 			while (count > 0 && span != null)
 			{
-				var len = span.Length > count ? count : span.Length;
+				var len = span.Length - start;
+				len = len > count ? count : len;
 				var temp = span.Buffer == 0
 					? GetBufferFromMapped(span.Offset + start, (int)len)
 					: GetBufferFromEdit(span.Offset + start, (int)len);
@@ -45,7 +52,9 @@ namespace TextEditor.Core
 				span = span.Next;
 			}
 
-			return c - count;
+			var readCount = c - count;
+			_position += readCount;
+			return readCount;
 		}
 
 		private byte[] GetBufferFromEdit(long offset, int length)
@@ -64,53 +73,24 @@ namespace TextEditor.Core
 
 		private byte[] GetBufferFromMapped(long offset, int length)
 		{
-			var len = length > MemBlockSize ? MemBlockSize : length;
 			var baseIdx = CalcIndexBase(offset);
 			if (baseIdx != _stream.PointerOffset)
 				_stream = _mapped.CreateViewStream(baseIdx, MemBlockSize);
 
+			var start = offset - baseIdx;
+			var len = (int)(_stream.Length - start);
+			len = len < length ? len : length;
+
 			var buff = new byte[len];
-			_stream.Position = offset - baseIdx;
+			_stream.Position = start;
 			var count = _stream.Read(buff, 0, len);
+			if (len == count)
+				return buff;
+
 			var temp = new byte[count];
 			Array.Copy(buff, 0, temp, 0, count);
 
 			return temp;
-		}
-
-		public DocumentView GetNextView()
-		{
-			Span span;
-
-			var index = _spanTable.Find(++_viewIndex * OffsetFactor, out span);
-			if (index < 0)
-			{
-				_viewIndex--;
-				return null;
-			}
-
-			var idx = 0;
-			var buffer = new byte[MemBlockSize];
-			while (span != null)
-			{
-				var len = (int)Math.Min(MemBlockSize, span.Length);
-				if (span.Buffer == 0)
-				{
-					var s = _mapped.CreateViewStream(span.Offset, len);
-					var count = s.Read(buffer, idx, len);
-					idx += count;
-				}
-				else
-				{
-					_editBuffer.Position = span.Offset;
-					var count = _editBuffer.Read(buffer, idx, len);
-					idx += count;
-				}
-
-				span = span.Next;
-			}
-
-			return new DocumentView(this) { Offset = _viewIndex * OffsetFactor, Length = idx }.SetBuffer(buffer);
 		}
 
 		private static long CalcIndexBase(long index)
@@ -141,11 +121,16 @@ namespace TextEditor.Core
 
 		private void CreateEditBuffer()
 		{
-			var path = Path.GetDirectoryName(FileName);
+			var editBuff = GetNameWithPrefix(FileName, "working");
+			_editBuffer = new FileStream(editBuff, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
+		}
+
+		private static string GetNameWithPrefix(string fileName, string extension)
+		{
+			var path = Path.GetDirectoryName(fileName);
 			if (string.IsNullOrWhiteSpace(path))
 				path = Directory.GetCurrentDirectory();
-			var editBuff = Path.Combine(path, "$" + Path.GetFileNameWithoutExtension(FileName) + ".working");
-			_editBuffer = new FileStream(editBuff, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
+			return Path.Combine(path, "." + Path.GetFileNameWithoutExtension(fileName) + "." + extension);
 		}
 
 		private void InitSpanTable()
@@ -156,7 +141,52 @@ namespace TextEditor.Core
 
 		public void Save()
 		{
+			var saveBuff = GetNameWithPrefix(FileName, "saving");
+			using (var fs = new FileStream(saveBuff, FileMode.Create, FileAccess.Write, FileShare.None))
+			{
+				var span = _spanTable.Head;
+				while (span != null)
+				{
+					WriteMappedToStream(fs, span.Offset, span.Length, span.Buffer == 0);
+					span = span.Next;
+				}
+			}
 
+			File.Move(saveBuff, FileName);
+			_mapped.Dispose();
+			_editBuffer.Dispose();
+
+			Open();
+		}
+
+		public void WriteMappedToStream(Stream stream, long offset, long length, bool writeToMapped)
+		{
+			while (length > 0)
+			{
+				var source = writeToMapped ? (Stream)_stream : _editBuffer;
+				if (writeToMapped)
+				{
+					var idxBase = CalcIndexBase(offset);
+					if (idxBase != _stream.PointerOffset)
+					{
+						_stream = _mapped.CreateViewStream(idxBase, MemBlockSize);
+						source = _stream;
+					}
+					_stream.Position = offset - _stream.PointerOffset;
+				}
+				else
+					_editBuffer.Position = offset;
+
+				var len = length > BufferSize ? BufferSize : length;
+				var buffer = new byte[len];
+				var count = source.Read(buffer, 0, (int)len);
+				if (count <= 0)
+					break;
+
+				stream.Write(buffer, 0, count);
+				length -= count;
+				offset += count;
+			}
 		}
 
 		public void Close()
